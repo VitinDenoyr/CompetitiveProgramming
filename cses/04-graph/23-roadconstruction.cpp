f8b1bc1f2e02c78fb6b66ca17565192816d3f3bb
VitinDenoyr <vitordenoyr@gmail.com>
2024-12-15 10:29:50 -0300

    + Exemplo union find melhorado

--- cses/04-graph/23-roadconstruction.cpp
+++ cses/04-graph/23-roadconstruction.cpp
@@ -6,61 +6,41 @@ using namespace std;
 
 int n,m;
 
-struct UnionFind { //Union by rank -> upper bound for tree height, Union by size -> qtd. of elements
-	vector<int> parent;
-	vector<int> sizec;
-	/*vector<int> rank;*/
+struct UnionFind {
+	vector<int> v;
 	UnionFind(int n){
-		parent = vector<int> (n+1, -1);
-		sizec = vector<int> (n+1, 1);
-		/*rank = vector<int> (n+1, 0);*/
+		v = vector<int> (n+1, -1);
 	}
+
 	int find(int a){
-		int rt = a, nd0 = a;
-		while(parent[rt] != -1){
-			rt = parent[rt];
-		}
-		while(parent[nd0] != -1){
-			int nd1 = nd0;
-			nd0 = parent[nd0];
-			parent[nd1] = rt;
-		}
-		return rt;
+		if(v[a] < 0) return a;
+		v[a] = find(v[a]);
+		return v[a];
 	}
-	int unite(int a, int b){
+
+	int unite(int a, int b){ //Retorna se a união foi necessária
 		int pa = find(a), pb = find(b);
 		if(pa == pb) return 0;
-		if(sizec[pa] < sizec[pb]){
+		if(-v[pa] < -v[pb]){
 			swap(pa,pb);
 		}
-		parent[pb] = pa;
-		sizec[pa] += sizec[pb];
-		return sizec[pa];
+		v[pa] += v[pb];
+		v[pb] = pa;
+		return -v[pa];
 	}
-	/*bool unite(int a, int b){
-		int pa = find(a), pb = find(b);
-		if(pa == pb) return 0;
-		if(rank[pa] < rank[pb]){
-			swap(a,b); swap(pa,pb);
-		}
-		parent[pb] = pa;
-		rank[pa] += (rank[pa] == rank[pb]);
-		return true;
-	}*/
 };
 
 int main(){
 
-	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 	cin >> n >> m;
-	UnionFind u(n); int maxSize = 1, qtComps = n;
+	UnionFind u(n);
+	int qtComponents = n, mx = 1;
 	for(int i = 0; i < m; i++){
-		int x,y;
-		cin >> x >> y;
-		int ans = u.unite(x,y);
-		if(ans > 0) qtComps--;
-		maxSize = max(maxSize, ans);
-		cout << qtComps << " " << maxSize << "\n";
+		int x,y; cin >> x >> y;
+		int res = u.unite(x,y);
+		qtComponents -= (res > 0);
+		mx = max(mx, res);
+		cout << qtComponents << " " << mx << "\n";
 	}
 
 	return 0;

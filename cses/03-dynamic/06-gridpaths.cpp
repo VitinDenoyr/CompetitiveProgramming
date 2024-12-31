0c3c3085b1f67695a6713116f05892783a63c23a
VitinDenoyr <vitordenoyr@gmail.com>
2024-12-31 16:50:02 -0300

    + Grid paths

--- cses/03-dynamic/06-gridpaths.cpp
+++ cses/03-dynamic/06-gridpaths.cpp
@@ -1,43 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define MAXN 1002
-#define mod 1000000007
-char tab[MAXN][MAXN];
-int qt[MAXN][MAXN];
- 
+const ll mod = 1000000007ll;
+
+vector<vector<ll>> tab, qt;
+
 int main(){
- 
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
- 
+
     int n; cin >> n;
-    for(int i = 0; i <= n+1; i++){
-        tab[i][0] = '*';
-        tab[0][i] = '*';
-        tab[n+1][i] = '*';
-        tab[i][n+1] = '*';
-    }
+	tab = vector<vector<ll>>(n+2, vector<ll>(n+2,0));
+	qt = vector<vector<ll>>(n+2, vector<ll>(n+2,0));
+
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
-            cin >> tab[i][j];
+            char c; cin >> c;
+			if(c == '.') tab[i][j] = 1;
         }
     }
-    qt[1][1] = 1;
-    if(tab[1][1] == '*'){
-        cout << 0 << "\n";
-        return 0;
-    }
+
+    qt[1][1] = tab[1][1]; tab[1][1] = 0;
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
-            if(tab[i][j] == '*') continue;
-            if(tab[i-1][j] == '.'){
-                qt[i][j] += qt[i-1][j];
-            }
-            if(tab[i][j-1] == '.'){
-                qt[i][j] += qt[i][j-1];
-            }
-            qt[i][j] %= mod;
+            if(!tab[i][j]) continue;
+            qt[i][j] = (qt[i][j-1] + qt[i-1][j])%mod;
         }
     }
     cout << qt[n][n] << "\n";
